aa65e0103848c996989d04f64579212fe810f39a
abdulhakim84 <54586446+abdulhakim84@users.noreply.github.com>
2026-08-28 18:59:51 +0700

    Modify time synchronization and display update logic
    Updated NTP server and time offset for Indonesia. Changed display update method to use the new Display class.

--- src/TimeUpdater.cpp
+++ src/TimeUpdater.cpp
@@ -2,26 +2,35 @@
 #include <NTPClient.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
-
-#include "LvglDisplay.h"
+#include "Display.h"
 
 unsigned long TimeUpdater::_lastMinute = -1;
 WiFiUDP udp;
-auto timeClient = NTPClient(udp, "time1.aliyun.com");
+auto timeClient = NTPClient(udp, "ntp.aliyun.com");
 
 void TimeUpdater::begin() {
     timeClient.begin();
-    timeClient.setTimeOffset(28800); // 设置时区，这里是北京时间（UTC+8），8 * 60 * 60 = 28800
+    
+    // Offset Waktu Indonesia:
+    // WIB  (UTC+7) = 7 * 3600 = 25200
+    // WITA (UTC+8) = 8 * 3600 = 28800
+    // WIT  (UTC+9) = 9 * 3600 = 32400
+    timeClient.setTimeOffset(25200); 
+
     xTaskCreate([](void *ptr) {
         while (true) {
-            timeClient.update(); // update函数内部做了更新间隔检测，并不会每一次都从网络获取时间
+            timeClient.update();
             const unsigned long localEpochTime = timeClient.getEpochTime();
             const unsigned long currentHour = (localEpochTime % 86400) / 3600;
             const unsigned long currentMinute = (localEpochTime % 3600) / 60;
+            
             if (currentMinute != _lastMinute) {
                 char timeStr[6];
                 snprintf(timeStr, sizeof(timeStr), "%02lu:%02lu", currentHour, currentMinute);
-                LvglDisplay::updateTime(timeStr);
+                
+                // Pembaruan waktu ke OLED
+                Display::updateTime(timeStr);
+                
                 _lastMinute = currentMinute;
             }
             vTaskDelay(pdMS_TO_TICKS(1000));
