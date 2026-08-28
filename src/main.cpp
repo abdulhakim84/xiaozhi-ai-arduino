ee9890299c15b8196f4efc3f7b7c5ee79ecbd5b7
abdulhakim84 <54586446+abdulhakim84@users.noreply.github.com>
2026-08-28 19:18:32 +0700

    Refactor display initialization and update comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,37 @@
 #include "Application.h"
 #include "Arduino.h"
 #include "GlobalState.h"
-#include "LvglDisplay.h"
+#include "Display.h"
 #include "Settings.h"
 #include "WiFiManager.h"
 
 static auto TAG = "Main";
 
 void setup() {
-    // 加载本地配置信息
+    // 1. Memuat konfigurasi lokal dari SPIFFS & NVS Flash
     Settings::begin();
-    // 显示屏幕
-    LvglDisplay::begin();
-    // 连接网络
+
+    // 2. Inisialisasi Layar OLED SSD1306 (SDA = 41, SCL = 42)
+    Display::begin(41, 42);
+
+    // 3. Memulai proses koneksi jaringan Wi-Fi
     WiFiManager::setupWiFi();
 
-    ESP_LOGD(TAG, "等待网络连接...");
-    // 等待网络连接成功
-    xEventGroupWaitBits(GlobalState::getEventGroup(), GlobalState::getEventBits({NetworkConnected}),
+    ESP_LOGD(TAG, "Mengecek status koneksi jaringan...");
+    
+    // 4. Menunggu sampai Wi-Fi berhasil terhubung
+    xEventGroupWaitBits(GlobalState::getEventGroup(), 
+                        GlobalState::getEventBits({NetworkConnected}),
                         false, true, portMAX_DELAY);
-    ESP_LOGD(TAG, "网络连接成功");
+
+    ESP_LOGD(TAG, "Jaringan terhubung!");
     GlobalState::setState(Sleep);
-    // 启动聊天主流程
+
+    // 5. Jalankan layanan utama AI (Audio Player, Recording, TTS/ASR)
     Application::getInstance().begin();
 }
 
 void loop() {
+    // FreeRTOS menangani seluruh task di background
+    vTaskDelay(pdMS_TO_TICKS(1000));
 }
